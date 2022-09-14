bfe108b9227e5127a6da06beb81f1b31a84233bf
Morsladio <52522895+Morsladio@users.noreply.github.com>
2022-09-14 11:44:08 -0500

    Add files via upload

---
+++ Potencias.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Potencias{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int numero, cuadrado, cubo, cuarta, quinta;
+
+		System.out.print("Ingrese un numero: ");
+		numero = in.nextInt();
+
+		if (numero>=1 && numero<=25){
+			cuadrado = numero * numero;
+			cubo = numero * numero * numero;
+			cuarta = numero * numero * numero * numero;
+			quinta = numero * numero * numero * numero * numero;
+
+			System.out.println("El numero es: " + numero);
+			System.out.println("El cuadrado es: " + cuadrado);
+			System.out.println("El cubo es: " + cubo);
+			System.out.println("La cuarta es: " + cuarta);
+			System.out.println("La quinta es: " + quinta); 
+			}			
+	}
+}
+
+
+
+
+/*Al cuadrado
+al cubo
+a la cuarta
+a la quinta
+*/
